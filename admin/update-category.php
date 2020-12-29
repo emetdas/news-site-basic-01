f3c752b8bdaf775278ab951fd1dc4505a54902d1
emetdas <emet@b2bleadstaff.com>
2020-12-29 21:27:45 +0600

    update is sussfully

--- admin/update-category.php
+++ admin/update-category.php
@@ -1,4 +1,20 @@
-<?php include "header.php"; ?>
+<?php 
+include "header.php"; 
+include "config.php";
+$get_id = mysqli_real_escape_string($con,$_GET['id']);
+$select = "SELECT * FROM category WHERE category_id = {$get_id}";
+$query = mysqli_query($con,$select) or die("Data not selected");
+if (isset($_POST['sumbit'])) {
+    $category_name = mysqli_real_escape_string($con,$_POST['cat_name']);
+    $update = "UPDATE category SET category_name ='{$category_name}' WHERE category_id = {$get_id}";
+    if (mysqli_query($con,$update)) {
+        header("location:category.php");
+    }
+    else{
+        echo "Data not Update";
+    }
+}
+?>
   <div id="admin-content">
       <div class="container">
           <div class="row">
@@ -6,16 +22,24 @@
                   <h1 class="adin-heading"> Update Category</h1>
               </div>
               <div class="col-md-offset-3 col-md-6">
-                  <form action="" method ="POST">
+                  <form action="<?php $_SERVER['PHP_SELF']?>" method ="POST">
+                  <?php
+                  if (mysqli_num_rows($query) > 0) {
+                      while ($row = mysqli_fetch_assoc($query)) {
+                         
+                 ?>
                       <div class="form-group">
-                          <input type="hidden" name="cat_id"  class="form-control" value="1" placeholder="">
+                          <input type="hidden" name="cat_id"  class="form-control" value="<?php echo $get_id;?>" placeholder="">
                       </div>
                       <div class="form-group">
                           <label>Category Name</label>
-                          <input type="text" name="cat_name" class="form-control" value="Html"  placeholder="" required>
+                          <input type="text" name="cat_name" class="form-control" value="<?php echo $row['category_name'];?>"  placeholder="" required>
                       </div>
                       <input type="submit" name="sumbit" class="btn btn-primary" value="Update" required />
-                  </form>
+                <?php      }
+                  }
+                  ?>
+                    </form>
                 </div>
               </div>
             </div>
