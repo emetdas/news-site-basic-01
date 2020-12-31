b70be08261556e03ef255c4e97060a42d4f2ef41
emetdas <emet@b2bleadstaff.com>
2020-12-31 22:32:01 +0600

    update

--- admin/save-post.php
+++ admin/save-post.php
@@ -1,12 +1,27 @@
 <?php
 include "config.php";
 if (isset($_POST['fileToUpload'])) {
+    $errors = array();
+    $file_name = $_FILES['fileToUpload']['name']; 
+    $file_size = $_FILES['fileToUpload']['size']; 
+    $temp_name = $_FILES['fileToUpload']['tmp_name']; 
+    $file_type = $_FILES['fileToUpload']['type']; 
+    $file_ext = strtolower(end(explode('.',$file_name))); 
+    $exctions = array("jpeg","jpg","svg","png");
+    if (in_array($file_ext,$exctions) === false) {
+        $errors[] = "This exctions file is not alowed, Please Chose jpeg,jpg,svg,png exctions";
+    }
+    if ($file_size > 2097152) {
+        $errors[] = "File size must be lesten 2MB or lower";
+    }
+    
+}
     $title = mysqli_real_escape_string($con,$_POST['post_title']);
     $descpection = mysqli_real_escape_string($con,$_POST['postdesc']);
     $category = mysqli_real_escape_string($con,$_POST['category']);
     $date = date("d M,Y");
+    $_SESSION["user_id"];
     $other = mysqli_real_escape_string($con,$_POST['category']);
-    $fileToUpload = mysqli_real_escape_string($con,($_POST['fileToUpload']));
     $sql = "SELECT * FROM";
     $query = mysqli_query($con,$sql);
    if (mysqli_num_rows($query) > 0 ) {
@@ -18,5 +33,5 @@ if (isset($_POST['fileToUpload'])) {
           header("location:users.php");
        }
    }
-}
+
 ?>
