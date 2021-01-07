03cac77a381a6c57edd310cccf1a8f9c7dd5fad8
emetdas <emet@b2bleadstaff.com>
2021-01-07 22:11:40 +0600

    sever

--- admin/delete-post.php
+++ admin/delete-post.php
@@ -1,9 +1,10 @@
 <?php
-include "header.php";
 include "config.php";
 $get_id = $_GET['id'];
-$delet = "DELETE FROM post WHERE post_id = {$get_id}";
-if (mysqli_query($con,$delet)) {
+$cat_id = $_GET['catid'];
+$delet = "DELETE FROM post WHERE post_id = {$get_id};";
+$delet .= "UPDATE category SET post = post - 1 WHERE category_id = {$cat_id}";
+if(mysqli_multi_query($con,$delet)){
     header("location:post.php");
 }
 else{
--- admin/post.php
+++ admin/post.php
@@ -52,7 +52,7 @@ $result = mysqli_query($con, $select) or die("no data found");
                               <td><?php echo $row['post_date'];?></td>
                               <td><?php echo $row['username'];?></td>
                               <td class='edit'><a href='update-post.php?id=<?php echo $row['post_id'];?>'><i class='fa fa-edit'></i></a></td>
-                              <td class='delete'><a href='delete-post.php?id=<?php echo $row['post_id'];?>'><i class='fa fa-trash-o'></i></a></td>
+                              <td class='delete'><a href='delete-post.php?id=<?php echo $row['post_id'];?>&catid=<?php echo $row['category'];?>'><i class='fa fa-trash-o'></i></a></td>
                           </tr>
                           <?php       
                                 }
