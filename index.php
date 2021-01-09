19c60da50f82d4da04287cc812c065d1d4bd2cf7
emetdas <emet@b2bleadstaff.com>
2021-01-09 18:08:46 +0600

    commit change

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<?php include 'header.php'; ?>
+<?php 
+include 'header.php'; 
+include 'config.php'; 
+?>
     <div id="main-content">
         <div class="container">
             <div class="row">
